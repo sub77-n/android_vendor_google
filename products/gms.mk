334315a6c35d5d3ca9ae990a9ecd2d98443d1b74
David Viteri <davidteri91@gmail.com>
2016-01-12 23:49:38 +0100

    bootanimation: fix ifneq for CUSTOM_BUILD
    @erikcas it was only tested on 480 DPI targets
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- products/gms.mk
+++ products/gms.mk
@@ -3,7 +3,7 @@ ROM_BUILDTYPE := HOMEMADE
 ROM_VERSION := $(TARGET_PRODUCT)-$(shell date -u +%Y%m%d)-$(PLATFORM_VERSION)-$(BUILD_ID)-$(ROM_BUILDTYPE)
 
 # bootanimation (480 DPI)
-ifneq ($(filter honami leo sirius sumire, $(TARGET_DEVICE)),)
+ifneq ($(filter aosp_c6903 aosp_d6503 aosp_d6603 aosp_e6653 aosp_e6853, $(TARGET_PRODUCT)),)
 
 PRODUCT_COPY_FILES +=  \
     vendor/google/prebuilt/common/bootanimation/480/bootanimation.zip:system/media/bootanimation.zip
