3e92f44f4d0aa766f36065d2f367d66256afc2d1
David Viteri <davidteri91@gmail.com>
2016-01-12 21:41:53 +0100

    vendor: initial commit
    Signed-off-by: David Viteri <davidteri91@gmail.com>

---
+++ products/gms.mk
@@ -0,0 +1,11 @@
+# version
+ROM_BUILDTYPE := HOMEMADE
+ROM_VERSION := $(TARGET_DEVICE)-$(shell date -u +%Y%m%d)-$(PLATFORM_VERSION)-$(BUILD_ID)-$(ROM_BUILDTYPE)
+
+# bootanimation (480 DPI)
+ifneq ($(filter honami leo sirius sumire, $(TARGET_DEVICE)),)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/google/prebuilt/common/bootanimation/480/bootanimation.zip:system/media/bootanimation.zip
+
+endif
