32a48c5629fca7c36fb9c406e9e495d11a87d704
David <Davidteri91@gmail.com>
2016-06-10 00:47:44 +0200

    Vendor-google: add BuildScript Sample for users
    too many users are having issues building ... add this basic script to build AOSP for their Xperias
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# ccache
+export USE_CCACHE=1
+export KBUILD_BUILD_USER=$USER
+
+# erikcas: lower cchace
+prebuilts/misc/linux-x86/ccache/ccache -M 32G
+
+# INFO: before start to build change $device-userdebug
+# with your device model.... Ex. c6903-userdebug for
+# build AOSP for Sony Xperia Z1 (honami - c6903)......
+
+repo sync
+
+rm -rf out
+. build/envsetup.sh
+lunch aosp_$device-userdebug
+
+# ENABLED by default
+make -j$(grep -c ^processor /proc/cpuinfo) dist
+
+# ENABLE this for make recovery and kernel images
+#make recoveryimage
